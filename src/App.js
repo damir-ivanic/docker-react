d99104493469aed72e9750ba5a7697cf176c7dab
damir-ivanic <58272112+damir-ivanic@users.noreply.github.com>
2024-01-31 21:12:57 +0100

    Demo for Dario

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi there MARKO!</p>
+        <p>Hi there Dario!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
